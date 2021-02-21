3a2498497db4e4037539dcdff88d1f98437a4f5d
joaofernandes999 <dotagt844@gmail.com>
2021-02-21 10:24:45 -0300

    Primeiro Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
 
-function App() {
+const App = ()=>{
+  const [countries,setcountries] = useState([]);
+
+  const pesquisa = async() => {
+    const nameCountrie = document.getElementById('campoPesquisa').value;
+    if(nameCountrie != '' && nameCountrie != null){
+      const response = await fetch('https://restcountries.eu/rest/v2/name/' + nameCountrie)
+      const data = await response.json();
+      if(data.status == 404){
+        alert('Pais não encontrado');
+      }else{
+        setcountries(data);
+        console.log(data)
+      }
+    }else{
+      alert('Nome não pode ser vazio');
+    }
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+     <div class='divPesquisa'>
+        <label>Digite o nome do Pais: </label>
+        <input id='campoPesquisa' type='text'></input> 
+        <button onClick={pesquisa}>Pesquisar</button>
+     </div>
+     <div class='paises'>
+      {countries.map(c => (
+        <>
+        <h1>{c.name}</h1>
+        <img src={c.flag}></img>
+
+        <div class='results'>
+          <label><h3>Capital:</h3></label>
+          <h4>{c.capital}</h4>
+        </div>
+
+        <div class='results'>
+          <label><h3>Região:</h3></label>
+          <h4>{c.region}</h4>
+        </div>
+
+        <div class='results'>
+          <label><h3>População:</h3></label>
+          <h4>{c.population}</h4>
+        </div>
+
+        <div class='results'>
+          <label><h3>Lingua:</h3></label>
+          <h4>{c.languages[0].name}</h4>
+        </div>
+        
+        </>
+      ))}
+     </div>
+   </div>
   );
 }
 
